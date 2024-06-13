ef9f93fe613c77d487768142cd9cf649e9a45c73
Shlomi Yahbes <syahbes@gmail.com>
2024-06-13 23:55:39 +0300

    first screen

--- app/(tabs)/form.tsx
+++ app/(tabs)/form.tsx
@@ -1,20 +1,60 @@
-import { ThemedText } from "@/components/ThemedText";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
+    <ScrollView>
+      <View style={styles.container}>
         <Image
           source={require("@/assets/images/car/elementsServiceIconsCharger.png")}
           style={styles.image}
         />
+        <Text style={styles.bigText}>פרטים</Text>
+        <TextInput
+          value={description}
+          onChangeText={setDescription}
+          placeholder="תיאור / הערות"
+          multiline={true}
+          numberOfLines={5}
+          style={styles.inputText}
+        />
+        <Text style={styles.mediumText}>הוספת תמונות</Text>
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.flatList}
+          data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+          renderItem={({ item }) => (
+            <Image
+              source={require("@/assets/images/car/elements24PxIconsPlaceHolderImage2.png")}
+              style={styles.flatListImage}
+            />
+          )}
+          keyExtractor={(item) => item.toString()}
+        />
+        <TextInput
+          value={location}
+          onChangeText={setLocation}
+          placeholder="מיקום נוגכחי / הוראות הגעה"
+          multiline={true}
+          numberOfLines={5}
+          style={styles.inputText}
+        />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
-//create styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,14 +63,47 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
-  },
-  imageContainer: {
-    marginTop: 30,
-    width: 120,
-    height: 120,
+    padding: 20,
   },
   image: {
     width: 120,
     height: 120,
+  },
+  bigText: {
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+    alignContent: "flex-start",
+    width: "100%",
+  },
+  mediumText: {
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+    alignContent: "flex-start",
+    width: "100%",
+  },
+  inputText: {
+    marginTop: 10,
+    width: "100%",
+    height: 150,
+    textAlignVertical: "top",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 10,
+    padding: 10,
+  },
+  flatList: {
+    marginBottom: 10,
+    width: "100%",
+    height: 150,
+  },
+  flatListImage: {
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+    marginRight: 5,
+    backgroundColor: "#000",
   },
 });
